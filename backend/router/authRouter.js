d9fb28e2a37c363f3d87d30c9eda80a77b7b7438
harsh <harsh@harshs-MacBook-Air.local>
2025-03-26 19:14:57 +0530

    adding routes

---
+++ backend/router/authRouter.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const db = require("../dataBase/db");
+const router = express.Router();
+
+
+router.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  db.query(
+    "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
+    [name, email, hashedPassword],
+    (err, result) => {
+      if (err) return res.status(500).json({ error: err.message });
+      res.json({ message: "User registered successfully!" });
+    }
+  );
+});
+
+router.post("/login", (req, res) => {
+  const { email, password } = req.body;
+
+  db.query("SELECT * FROM users WHERE email = ?", [email], async (err, results) => {
+    if (err) return res.status(500).json({ error: err.message });
+    if (results.length === 0) return res.status(401).json({ message: "User not found!" });
+
+    const user = results[0];
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) return res.status(401).json({ message: "Invalid credentials" });
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT, { expiresIn: "1h" });
+    res.json({ message: "Login successful!", token });
+  });
+});
+
+const verifyToken = (req, res, next) => {
+  const token = req.headers["authorization"];
+  if (!token) return res.status(403).json({ message: "No token provided" });
+
+  jwt.verify(token, process.env.JWT, (err, decoded) => {
+    if (err) return res.status(401).json({ message: "Unauthorized",error: err });
+    req.userId = decoded.id;
+    next();
+  });
+};
+
+router.get("/profile", verifyToken, (req, res) => {
+  db.query("SELECT id, name, email FROM users WHERE id = ?", [req.userId], (err, result) => {
+    if (err) return res.status(500).json({ error: err.message });
+    res.json(result[0]);
+  });
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 
+app.use("/api/auth", authRoutes);
   
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
