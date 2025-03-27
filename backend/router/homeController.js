a80e4051cd13452dc1a425d0f80bcbf4cca73c72
harshjindal97 <hasrh946643@gmail.com>
2025-03-27 15:29:31 +0530

    working on frontend

--- backend/router/homeController.js
+++ backend/router/homeController.js
@@ -4,10 +4,10 @@ const verifyToken = require("../middleware/verifyJWT");
 const router = express.Router();
 
 router.get("/profile", verifyToken, (req, res) => {
-    db.query("SELECT id, name, email FROM users WHERE id = ?", [req.userId], (err, result) => {
-      if (err) return res.status(500).json({ error: err.message });
-      res.json(result[0]);
-    });
+  db.query("SELECT id, name, email FROM users WHERE id = ?", [req.userId], (err, result) => {
+    if (err) return res.status(500).json({ error: err.message });
+    res.json(result[0]);
   });
+});
 
 module.exports = router;
